709319b9a6650fdb206211002583aed64b543412
Lenovo <Lenovo@Aman-Anand>
2022-03-23 17:31:05 +0530

    push send time to Repo

---
+++ com.crm.vtiger/src/test/java/com/crm/vtiger/practice/UpdateLeadsTest.java
@@ -0,0 +1,12 @@
+package com.crm.vtiger.practice;
+
+import org.testng.annotations.Test;
+
+public class UpdateLeadsTest {
+	@Test
+	public void updatelead() {
+		System.out.println("Lead ");
+	}
+	
+
+}
